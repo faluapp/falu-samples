937121edc7f999fd43dd831de41cd5972109ccb1
Seth Onyango <57979366+sethonyango@users.noreply.github.com>
2024-03-13 09:22:34 +0300

    Migrate from Django to Fast Api (#30)

---
+++ python/models.py
@@ -0,0 +1,52 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class IdentityVerificationOptionsForDocument(BaseModel):
+	allowed: List[str]
+
+
+class IdentityVerificationOptionsForSelfie(BaseModel):
+	pass
+
+
+class IdentityVerificationOptionsForVideo(BaseModel):
+	poses: List[str]
+	recital: int
+
+
+class IdentityVerificationOptionsForIdNumber(BaseModel):
+	pass
+
+
+class IdentityVerificationOptions(BaseModel):
+	allow_uploads: Optional[bool] = None
+	id_number: Optional[IdentityVerificationOptionsForIdNumber] = None
+	document: Optional[IdentityVerificationOptionsForDocument] = None
+	selfie: Optional[IdentityVerificationOptionsForSelfie] = None
+	video: Optional[IdentityVerificationOptionsForVideo] = None
+
+
+class IdentityVerificationCreation(BaseModel):
+	type: str
+	return_url: Optional[str] = None
+	options: IdentityVerificationOptions()
+
+
+class IdentityVerification(BaseModel):
+	id: str
+	status: Optional[str] = None
+	type: Optional[str] = None
+	options: Optional[IdentityVerificationOptions] = None
+	client_secret: Optional[str] = None
+	url: Optional[str] = None
+	reports: Optional[List[str]] = None
+
+
+class TemporaryKey(BaseModel):
+	id: str
+	secret: str
+	workspace: str
+	objects: List[str]
+	expires: str
--- python/settings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from identity import views
-
-urlpatterns = [
-	path('identity/create-verification/', views.IdentityVerificationCreationView.as_view()),
-]
