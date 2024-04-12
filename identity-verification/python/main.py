4ea473515a9945439f5bf237f985225c9c924247
mburumaxwell <1645026+mburumaxwell@users.noreply.github.com>
2024-04-12 09:48:28 +0000

    Fix whitespaces

--- identity-verification/python/main.py
+++ identity-verification/python/main.py
@@ -12,37 +12,37 @@ app = FastAPI()
 
 @app.get("/ping")
 async def ping():
-	return {"ping": "pong!"}
+        return {"ping": "pong!"}
 
 
 @app.post("/identity/create-verification", response_model=IdentityVerification)
 async def create_verification(verification: IdentityVerificationCreation):
-	verification, error = falu.IdentityVerification.create_identity_verification(data=verification.dict())
+        verification, error = falu.IdentityVerification.create_identity_verification(data=verification.dict())
 
-	if verification and verification.resource is not None:
-		temp_key = create_key(verification.id)
+        if verification and verification.resource is not None:
+                temp_key = create_key(verification.id)
 
-		if temp_key is None:
-			raise HTTPException(status_code=400, detail="Key generation failed")
+                if temp_key is None:
+                        raise HTTPException(status_code=400, detail="Key generation failed")
 
-		data = verification.resource
-		data["temporary_key"] = temp_key
-		return data
-	else:
-		raise HTTPException(status_code=error.status_code, detail=error.problem)
+                data = verification.resource
+                data["temporary_key"] = temp_key
+                return data
+        else:
+                raise HTTPException(status_code=error.status_code, detail=error.problem)
 
 
 def create_key(identity_verification):
-	request = {
-		"identity_verification": identity_verification
-	}
+        request = {
+                "identity_verification": identity_verification
+        }
 
-	temporary_key, error = falu.TemporaryKey.create_temporary_key(data=request)
+        temporary_key, error = falu.TemporaryKey.create_temporary_key(data=request)
 
-	if temporary_key is not None:
-		if temporary_key.secret is not None:
-			return temporary_key.secret
-		else:
-			return None
+        if temporary_key is not None:
+                if temporary_key.secret is not None:
+                        return temporary_key.secret
+                else:
+                        return None
 
-	return None
+        return None
--- identity-verification/python/models.py
+++ identity-verification/python/models.py
@@ -4,50 +4,50 @@ from pydantic import BaseModel
 
 
 class IdentityVerificationOptionsForDocument(BaseModel):
-	allowed: List[str]
+        allowed: List[str]
 
 
 class IdentityVerificationOptionsForSelfie(BaseModel):
-	pass
+        pass
 
 
 class IdentityVerificationOptionsForVideo(BaseModel):
-	poses: List[str]
-	recital: int
+        poses: List[str]
+        recital: int
 
 
 class IdentityVerificationOptionsForIdNumber(BaseModel):
-	pass
+        pass
 
 
 class IdentityVerificationOptions(BaseModel):
-	allow_uploads: Optional[bool] = None
-	id_number: Optional[IdentityVerificationOptionsForIdNumber] = None
-	document: Optional[IdentityVerificationOptionsForDocument] = None
-	selfie: Optional[IdentityVerificationOptionsForSelfie] = None
-	video: Optional[IdentityVerificationOptionsForVideo] = None
+        allow_uploads: Optional[bool] = None
+        id_number: Optional[IdentityVerificationOptionsForIdNumber] = None
+        document: Optional[IdentityVerificationOptionsForDocument] = None
+        selfie: Optional[IdentityVerificationOptionsForSelfie] = None
+        video: Optional[IdentityVerificationOptionsForVideo] = None
 
 
 class IdentityVerificationCreation(BaseModel):
-	type: str
-	return_url: Optional[str] = None
-	options: IdentityVerificationOptions()
+        type: str
+        return_url: Optional[str] = None
+        options: IdentityVerificationOptions()
 
 
 class IdentityVerification(BaseModel):
-	id: str
-	status: Optional[str] = None
-	type: Optional[str] = None
-	options: Optional[IdentityVerificationOptions] = None
-	client_secret: Optional[str] = None
-	url: Optional[str] = None
-	reports: Optional[List[str]] = None
-	temporary_key: str
+        id: str
+        status: Optional[str] = None
+        type: Optional[str] = None
+        options: Optional[IdentityVerificationOptions] = None
+        client_secret: Optional[str] = None
+        url: Optional[str] = None
+        reports: Optional[List[str]] = None
+        temporary_key: str
 
 
 class TemporaryKey(BaseModel):
-	id: str
-	secret: str
-	workspace: str
-	objects: List[str]
-	expires: str
+        id: str
+        secret: str
+        workspace: str
+        objects: List[str]
+        expires: str
